0d262d98d78f4ce64b4ade2f4f3fd15095e93f1f
Kunal Sharma <kunalsharma@Kunals-MacBook-Pro.local>
2021-09-12 01:09:18 +0400

    travis build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Kunal there!</p>
+        <p>Why Kunal Babu ji is there!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
