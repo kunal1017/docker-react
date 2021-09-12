29b3e25d97aee3ee8fcef9999a8a23586541a6f9
Kunal Sharma <kunalsharma@Kunals-MacBook-Pro.local>
2021-09-12 11:42:57 +0400

    port0080

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Why Kunal ji is there!</p>
+        <p>Hello!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
